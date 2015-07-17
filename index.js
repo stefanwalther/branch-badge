ab0114efd32061177f2c4ec868fb2d2849a68c80
Stefan Walther <swr@qlik.com>
2015-07-17 12:28:00 +0200

    Initial commit

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 'use strict';
+var fs = require('fs');
 var express = require('express');
 var http = require('http');
 
@@ -8,8 +9,16 @@ var port = (process.env.PORT || 3000);
 app.set('port', (process.env.PORT || 5000));
 app.use( express.static(__dirname, + '/public'));
 
-app.get('/branch', function ( req, res ) {
-	console.log('req', req);
+app.get('/', function ( req, res ) {
+	if (req.header && req.header('Referer') && req.header('Referer' ).indexOf('branch.qlik.com') > -1) {
+		var img = fs.readFileSync('./public/branch.gif');
+		res.writeHead( 200, {'Content-Type': 'image/gif'});
+		res.end( img, 'binary');
+	} else {
+		var img = fs.readFileSync('./public/single-pix.gif');
+		res.writeHead( 200, {'Content-Type': 'image/gif'});
+		res.end( img, 'binary');
+	}
 });
 
 var server = app.listen( port, function (  ) {
